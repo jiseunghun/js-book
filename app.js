ab2e3a49ad90ef8111b3c82d4e4633733c5c1a77
Sunhye Moon <ss333567@gmail.com>
2021-12-31 09:14:43 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const hometown = [
+  { name: "남준", place: "일산", city: "고양" },
+  { name: "진", place: "과천" },
+  { name: "호석", place: "광주", city: "전라도" },
+  { name: "지민", place: "부산", city: "경상도" },
+];
+
+// for (let i = 0; i < hometown.length; i++) {
+//   const h = hometown[i];
+//   if (!h || !h.city) continue;
+
+//   console.log(`${i}번재 실행입니다.`);
+
+//   if (h.name === "호석") {
+//     console.log(`${h.name}의 고향은 ${h.city} ${h.place} 입니다.`);
+//     break;
+//   }
+// }
+
+// const store = { snack: 1000, flower: 4500, beverage: 2000, someting: "" };
+
+// for (let item in store) {
+//   if (!store.hasOwnProperty(item)) continue;
+
+//   console.log(`${item}는 가격이 ${store[item]} 입니다.`);
+// }
+
+
